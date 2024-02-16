50ef2a288f96f59b949afe49a297c3b3938f2a6c
Daniel Marin <danielmarinroman@gmail.com>
2024-02-16 16:55:04 +0100

    Arreglamos el método de invertirPalabra() y añadimos los test correspondientes del método

--- GestorPalabras/src/test/java/GestorPalabrasTest.java
+++ GestorPalabras/src/test/java/GestorPalabrasTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import static junit.framework.TestCase.*;
 public class GestorPalabrasTest {
     //Método contarVocales
-    
+    //Francisco
     @Test
     public void sinTildes() throws Exception {
         GestorPalabras ges = new GestorPalabras();
@@ -26,5 +26,48 @@ public class GestorPalabrasTest {
     public void tildesMayusculasYMinusculas() throws Exception {
         GestorPalabras ges = new GestorPalabras();
         assertEquals(3, ges.contarVocales("pátÁtáttttt"));
+    }
+    //Método InvertirPalabra
+    //Daniel
+    
+    @Test
+    public void sinTildes() throws Exception {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("aloh", gp.invertirPalabra("hola"));
+        assertEquals("inad", gp.invertirPalabra("dani"));
+    }
+    
+    @Test
+    public void conTildes() throws Exception {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("sóida", gp.invertirPalabra("adiós"));
+        assertEquals("lobrá", gp.invertirPalabra("árbol"));
+    }
+    
+    @Test
+    public void mayusculas() throws Exception {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("NAUJ", gp.invertirPalabra("JUAN"));
+        assertEquals("AMSI", gp.invertirPalabra("ISMA"));
+    }
+    
+    @Test
+    public void tildesMayusculasMinusculas() throws Exception {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("nóiva oravlÁ", gp.invertirPalabra("Álvaro avión"));
+    }
+    
+    @Test
+    public void numeros() throws Exception {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("4321", gp.invertirPalabra("1234"));
+        assertEquals("123", gp.invertirPalabra("321"));
+    }
+    
+    @Test
+    public void simbolos() throws Exception {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("&%$", gp.invertirPalabra("$%&"));
+        assertEquals(")(/", gp.invertirPalabra("/()"));
     }
 }
