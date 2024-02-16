b96056f672820e355c5fdd3c2947fb98893391b5
Leo-de-Torres <leodetorresorellana@gmail.com>
2024-02-16 17:03:47 +0100

    Arreglamos el Método esPalindromo

--- GestorPalabras/src/main/java/com/gestorpalabras/GestorPalabras.java
+++ GestorPalabras/src/main/java/com/gestorpalabras/GestorPalabras.java
@@ -4,8 +4,33 @@ public class GestorPalabras {
 
     public boolean esPalindromo(String palabra) {
         String palabraFormateada = palabra.replaceAll("\\s+", "").toLowerCase();
+        palabraFormateada = palabraFormateada.replaceAll("á", "a");
+        palabraFormateada = palabraFormateada.replaceAll("é", "e");
+        palabraFormateada = palabraFormateada.replaceAll("í", "i");
+        palabraFormateada = palabraFormateada.replaceAll("ó", "o");
+        palabraFormateada = palabraFormateada.replaceAll("ú", "u");
+        palabraFormateada = palabraFormateada.replaceAll("ü", "u");
+        palabraFormateada = palabraFormateada.replaceAll("ẃ", "w");
+        palabraFormateada = palabraFormateada.replaceAll("ŕ", "r");
+        palabraFormateada = palabraFormateada.replaceAll("ý", "y");
+        palabraFormateada = palabraFormateada.replaceAll("ṕ", "p");
+        palabraFormateada = palabraFormateada.replaceAll("ś", "s");
+        palabraFormateada = palabraFormateada.replaceAll("ǵ", "g");
+        palabraFormateada = palabraFormateada.replaceAll("j́", "j");
+        palabraFormateada = palabraFormateada.replaceAll("ḱ", "k");
+        palabraFormateada = palabraFormateada.replaceAll("ĺ", "l");
+        palabraFormateada = palabraFormateada.replaceAll("ź", "z");
+        palabraFormateada = palabraFormateada.replaceAll("ć", "c");
+        palabraFormateada = palabraFormateada.replaceAll("ǘ", "u");
+        palabraFormateada = palabraFormateada.replaceAll("ń", "n");
+        palabraFormateada = palabraFormateada.replaceAll("ḿ", "m");
+        
+        
 
         for (int i = 0; i < palabraFormateada.length() / 2; i++) {
+            if (!Character.isLetter(palabraFormateada.charAt(i))) {
+                return false;
+            }
             if (palabraFormateada.charAt(i) != palabraFormateada.charAt(palabraFormateada.length() - i - 1)) {
                 return false;
             }
