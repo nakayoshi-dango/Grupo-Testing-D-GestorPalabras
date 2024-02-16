ede778349e37474d1304bd84d97c43a40e18275f
nakayoshi-dango <fkfetan@gmail.com>
2024-02-16 16:46:45 +0100

    Corregido el método contarVocales() y añadidos sus tests. Añadidos los import necesarios.

--- GestorPalabras/src/test/java/GestorPalabrasTest.java
+++ GestorPalabras/src/test/java/GestorPalabrasTest.java
@@ -1,3 +1,30 @@
+import com.gestorpalabras.GestorPalabras;
+import org.junit.Test;
+import static junit.framework.TestCase.*;
 public class GestorPalabrasTest {
+    //Método contarVocales
     
+    @Test
+    public void sinTildes() throws Exception {
+        GestorPalabras ges = new GestorPalabras();
+        assertEquals(5, ges.contarVocales("murcielagojjjj"));
+    }
+
+    @Test
+    public void conTildes() throws Exception {
+        GestorPalabras ges = new GestorPalabras();
+        assertEquals(5, ges.contarVocales("murciélagojjjjj"));
+    }
+
+    @Test
+    public void mayusculas() throws Exception {
+        GestorPalabras ges = new GestorPalabras();
+        assertEquals(3, ges.contarVocales("PATATAqqqqq"));
+    }
+
+    @Test
+    public void tildesMayusculasYMinusculas() throws Exception {
+        GestorPalabras ges = new GestorPalabras();
+        assertEquals(3, ges.contarVocales("pátÁtáttttt"));
+    }
 }
